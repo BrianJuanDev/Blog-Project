e3367ace9c64b5dd1928c94e08fcb7d0308cd1b9
Brian <Brianjuandev@gmail.com>
2024-03-24 21:20:32 -0300

    managers added

--- myapp/models.py
+++ myapp/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                        .filter(status=Post.status.PUBLISHED)
 
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -15,6 +19,10 @@ class Post(models.Model):
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
+
+    objects = models.Manager()
+    published = PublishedManager() 
+
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
